4200ec5bd93748c051f24b03bde8909f8ceef464
Jitzek <Jitzek@users.noreply.github.com>
2020-03-17 19:43:16 +0100

    Login Update
    - Added more tests
    - Added check for locked user
    - Added error message variable
    - Added hard coded variables instead of accepting posted variables (given group)

--- isala.local/app/models/LoginModel.php
+++ isala.local/app/models/LoginModel.php
@@ -1,20 +1,28 @@
 <?php
 
 require_once('../app/ldap/connection.php');
+require_once('../app/database/connection.php');
 
 class LoginModel
 {
     private $title;
+    private $db;
     private $ldap;
     public function __construct()
     {
         $this->title = 'Login';
+        $this->db = new DBConnection();
         $this->ldap = new LDAPConnection();
     }
 
     public function getTitle()
     {
         return $this->title;
+    }
+
+    public function getDB()
+    {
+        return $this->db;
     }
 
     public function getLDAP()
--- isala.local/tests/_files/Login/LoginTest.php
+++ isala.local/tests/_files/Login/LoginTest.php
@@ -103,6 +103,15 @@ class LoginTest extends TestCase
         $this->assertFalse($this->attemptLogin($uid, $passwd, $group));
     }
 
+    /** @test */
+    public function attemptLogin_lockedAccount1()
+    {
+        $uid = 'locked';
+        $passwd = 'lock';
+        $group = 'patienten';
+        $this->assertFalse($this->attemptLogin($uid, $passwd, $group));
+    }
+
     public function attemptLogin($uid, $passwd, $group)
     {
         /**
@@ -119,19 +128,13 @@ class LoginTest extends TestCase
                 $this->returnCallback(function ($arg, $args) {
                     switch ($arg) {
                         case 'bind':
-                            if ($args[0] == NULL && $args[1] == NULL) {
-                                return true;
-                            } else if ($args[0] == 'cn=Elzen Knop,ou=developers,dc=isala,dc=local' && $args[1] == 'idebian') {
-                                return true;
-                            } else if ($args[0] == 'cn=Jan,ou=dokters,ou=isala,dc=isala,dc=local' && $args[1] == 'jan') {
-                                return true;
-                            } else if ($args[0] == 'cn=diederik,ou=dietisten,ou=isala,dc=isala,dc=local' && $args[1] == 'dieet') {
-                                return true;
-                            } else if ($args[0] == 'uid=admin,ou=administrators,ou=ccc,ou=isala,dc=isala,dc=local' && $args[1] == 'isaladebian') {
-                                return true;
-                            } else {
-                                return false;
-                            }
+                            if ($args[0] == NULL && $args[1] == NULL) return true;
+                            else if ($args[0] == 'cn=Elzen Knop,ou=developers,dc=isala,dc=local' && $args[1] == 'idebian') return true;
+                            else if ($args[0] == 'cn=Jan,ou=dokters,ou=isala,dc=isala,dc=local' && $args[1] == 'jan') return true;
+                            else if ($args[0] == 'cn=diederik,ou=dietisten,ou=isala,dc=isala,dc=local' && $args[1] == 'dieet') return true;
+                            else if ($args[0] == 'uid=admin,ou=administrators,ou=ccc,ou=isala,dc=isala,dc=local' && $args[1] == 'isaladebian') return true;
+                            else if ($args[0] == 'uid=locked,ou=patienten,dc=isala,dc=local' && $args[1] == 'lock') return true;
+                            return false;
                         case 'uidExists':
                             if ($args[1] == 'inetOrgPerson') {
                                 if ($args[0] != 'admin') return true;
@@ -149,6 +152,8 @@ class LoginTest extends TestCase
                                     return 'cn=diederik,ou=dietisten,ou=isala,dc=isala,dc=local';
                                 case 'admin':
                                     return 'uid=admin,ou=administrators,ou=ccc,ou=isala,dc=isala,dc=local';
+                                case 'locked':
+                                    return 'uid=locked,ou=patienten,dc=isala,dc=local';
                                 default:
                                     return '';
                             }
@@ -156,9 +161,6 @@ class LoginTest extends TestCase
                             switch ($args[0]) {
                                 case 'cn=developers,ou=developers,dc=isala,dc=local':
                                     if ($args[1] == 'cn=Elzen Knop,ou=developers,dc=isala,dc=local') return true;
-                                    return false;
-                                case 'cn=patienten,ou=patienten,dc=isala,dc=local':
-                                    if ($args[1] == '') return true;
                                     return false;
                                 case 'cn=dokters,ou=dokters,ou=isala,dc=isala,dc=local':
                                     if ($args[1] == 'cn=Jan,ou=dokters,ou=isala,dc=isala,dc=local') return true;
@@ -168,6 +170,9 @@ class LoginTest extends TestCase
                                     return false;
                                 case 'cn=administrators,ou=administrators,ou=ccc,ou=isala,dc=isala,dc=local':
                                     if ($args[1] == 'uid=admin,ou=administrators,ou=ccc,ou=isala,dc=isala,dc=local') return true;
+                                case 'cn=patienten,ou=patienten,dc=isala,dc=local':
+                                    if ($args[1] == 'uid=locked,ou=patienten,dc=isala,dc=local') return true;
+                                    return false;
                                 default:
                                     return false;
                             }
@@ -195,53 +200,115 @@ class LoginTest extends TestCase
                 })
             );
 
+        $db = $this->getMockBuilder(DBConnection::class)
+            ->getMock();
+
+        $db->method('getConnection')
+            ->willReturn(true);
+
+        $db->method('query')
+            ->will(
+                $this->returnCallback(function ($arg, $args) {
+                    switch ($arg) {
+                        case 'userIsLocked':
+                            if ($args[0] == 'elzenknopje') return false;
+                            else if ($args[0] == 'j.janssen' && $args[1] == 'Dokter') return false;
+                            else if ($args[0] == 'D.i.Eet' && $args[1] == 'Diëtist') return false;
+                            else if ($args[0] == 'admin') return false;
+                            else if ($args[0] == 'locked' && $args[1] == 'Patiënt') return true;
+                            return true;
+                    }
+                })
+            );
+
         $model = $this->getMockBuilder(LoginModel::class)
             ->getMock();
 
         $model->method('getLDAP')
             ->willReturn($ldap);
 
+        $model->method('getDB')
+            ->willReturn($db);
+
         /* ----- Done configuring Mocks ----- */
 
-        if ($model->getLDAP()->getConnection()) {
-            $ldapbind = $model->getLDAP()->query('bind', [NULL, NULL]); //NULL, NULL = anonymous bind
-            if (!$ldapbind) {
-                return false;
-            }
-            // Check if User Exists
-            if (!$model->getLDAP()->query('uidExists', [$uid, "inetOrgPerson"])
-                && !$model->getLDAP()->query('uidExists', [$uid, "account"])) return false;
-
-            // Get User's DN
-            $ldap_user_dn = $model->getLDAP()->query('getDnByUid', [$uid]);
-
-            // Check if User is in Group
-            if ($group == 'anders') {
-                $possible_groups = ["dietisten", "psychologen", "fysiotherapeuten", "administrators"];
-                foreach ($possible_groups as $possible_group) {
-                    $ldap_group_dn = $model->getLDAP()->query('getGroupDNByName', [$possible_group]); // Location of the group in LDAP Directory
-                    // TODO: search for unique member in unique group
-                    if ($model->getLDAP()->query('userInGroup', [$ldap_group_dn, $ldap_user_dn, "groupOfNames", "member"])) break;
-                    $ldap_group_dn = '';
-                }
-                if ($ldap_group_dn == '') return false;
-            } else {
-                $ldap_group_dn = $model->getLDAP()->query('getGroupDNByName', [$group]); // Location of the group in LDAP Directory
-                if (!$model->getLDAP()->query('userInGroup', [$ldap_group_dn, $ldap_user_dn, "groupOfNames", "member"])) return false;
-            }
-
-            // Bind to LDAP with this user
-            $ldapbind = $model->getLDAP()->query('bind', [$ldap_user_dn, $passwd]);
-            if (!$ldapbind) {
-                return false;
-            }
-
-            $_SESSION['uid'] = $uid;
-            return true;
-        } else {
-            die('Connection to LDAP service failed');
+        // Check for LDAP Connection
+        if (!$model->getLDAP()->getConnection()) {
+            // Display error message
+            return false;
         }
-        return false;
+        if (!$model->getDB()->getConnection()) {
+            // Display error message
+            return false;
+        }
+
+        if (!$model->getLDAP()->query('bind', [NULL, NULL])) return false;
+        // Check if User Exists
+        if (
+            !$model->getLDAP()->query('uidExists', [$uid, "inetOrgPerson"])
+            && !$model->getLDAP()->query('uidExists', [$uid, "account"])
+        ) return false;
+
+        // Get User's DN
+        $ldap_user_dn = $model->getLDAP()->query('getDnByUid', [$uid]);
+
+        // Check if User is in Group
+        if ($group == 'anders') {
+            $possible_groups = ["dietisten", "psychologen", "fysiotherapeuten", "administrators"];
+            foreach ($possible_groups as $possible_group) {
+                $ldap_group_dn = $model->getLDAP()->query('getGroupDNByName', [$possible_group]); // Location of the group in LDAP Directory
+                if ($model->getLDAP()->query('userInGroup', [$ldap_group_dn, $ldap_user_dn, "groupOfNames", "member"])) {
+                    // Check if account is locked
+                    $table = $this->getTableNameForPostedGroup($possible_group);
+                    if ($model->getDB()->query('userIsLocked', [$uid, $table])) {
+                        // Display error message
+                        return false;
+                    }
+                    break;
+                }
+                $ldap_group_dn = '';
+            }
+            if ($ldap_group_dn == '') return false;
+        } else {
+            if ($group != 'developers' && $group != 'patienten' && $group != 'dokters') return false; //TODO: remove developers
+            $ldap_group_dn = $model->getLDAP()->query('getGroupDNByName', [$group]); // Location of the group in LDAP Directory
+            if (!$model->getLDAP()->query('userInGroup', [$ldap_group_dn, $ldap_user_dn, "groupOfNames", "member"])) return false;
+
+            // Check if account is locked
+            $table = $this->getTableNameForPostedGroup($group);
+            if ($model->getDB()->query('userIsLocked', [$uid, $table])) {
+                // Display error message
+                return false;
+            }
+        }
+
+        // Bind to LDAP with this user (check password)
+        if (!$model->getLDAP()->query('bind', [$ldap_user_dn, $passwd])) return false;
+
+        $_SESSION['uid'] = $uid;
+        return true;
+    }
+
+    private function getTableNameForPostedGroup($group)
+    {
+        switch ($group) {
+            case 'patienten':
+                return 'Patiënt';
+            case 'dokters':
+                return 'Dokter';
+            case 'dietisten':
+                return 'Diëtist';
+            case 'dokters':
+                return 'Dokter';
+            case 'psychologen':
+                return 'Psycholoog';
+            case 'fysiotherapeuten':
+                return 'Fysiotherapeut';
+            case 'administrators':
+                return 'Admin';
+            default:
+                return '';
+        }
     }
 }
 
@@ -250,9 +317,24 @@ class LoginModel
     public function getLDAP()
     {
     }
+
+    public function getDB()
+    {
+    }
 }
 
 class LDAPConnection
+{
+    public function getConnection()
+    {
+    }
+
+    public function query()
+    {
+    }
+}
+
+class DBConnection
 {
     public function getConnection()
     {

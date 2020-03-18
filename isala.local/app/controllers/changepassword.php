c677f67b9a3c011f63a490c7abeff910b6886c2c
Jitzek <Jitzek@users.noreply.github.com>
2020-03-19 00:30:43 +0100

    Updated changepassword Controller

--- isala.local/app/controllers/changepassword.php
+++ isala.local/app/controllers/changepassword.php
@@ -6,14 +6,23 @@ class ChangePassword extends Controller
 {
     private $model;
     private $err_msg;
-    public function index($uid) // TODO: don't transfer password over URL
+    public function index() // TODO: don't transfer password over URL
     {
+        /*if (!$_SESSION['uid']) {
+            return header("Location: /public/login");
+        }*/
         // Define Model to be used
         $this->model = $this->model('ChangePasswordModel');
 
+        // Parse data to view
+        $this->view('changepassword/index', ['title' => $this->model->getTitle()]);
+
         if ($_POST['change_password']) {
-            if ($this->validatePasswordTest($_POST['prev_password'], $_POST['new_password'])) {
-                $this->attemptPasswordChange($uid, $_POST['prev_password'], $_POST['new_password']);
+            if ($this->validatePassword($_POST['prev_password'], $_POST['new_password'])) {
+                if (!$this->attemptPasswordChange($_SESSION['uid'], $_POST['prev_password'], $_POST['new_password'])) {
+                    if (strlen($this->err_msg) < 1) echo "<p style=\"color: #FC240F\">Something went wrong</p>";
+                    else echo "<p style=\"color: #FC240F\">" .  htmlentities($this->err_msg) . "</p>";
+                }
             } else {
                 echo "<p style=\"color: #FC240F\">" .  htmlentities($this->err_msg) . "</p>";
             }
@@ -22,35 +31,70 @@ class ChangePassword extends Controller
 
     protected function attemptPasswordChange($uid, $prev_password, $new_password)
     {
-        if ($this->model->getLDAP()->getConnection()) {
-            if (!$this->model->getLDAP()->query('bind', [NULL, NULL])) return false; //NULL, NULL = anonymous bind
+        if (!$this->model->getLDAP()->getConnection()) {
+            $this->err_msg = 'Connection Failed';
+            return false;
+        }
+        if (!$this->model->getDB()->getConnection()) {
+            $this->err_msg = 'Connection Failed';
+            return false;
+        }
 
-            // Check if User Exists
-            if (
-                !$this->model->getLDAP()->query('uidExists', [$uid, "inetOrgPerson"])
-                && !$this->model->getLDAP()->query('uidExists', [$uid, "account"])
-            ) return false;
+        if (!$this->model->getLDAP()->query('bind', [NULL, NULL])) return false; //NULL, NULL = anonymous bind
 
-            // Get user DN
-            $user_dn = $this->model->getLDAP()->query('getDnByUid', [$uid]);
+        // Check if User Exists
+        if (
+            !$this->model->getLDAP()->query('uidExists', [$uid, "inetOrgPerson"])
+            && !$this->model->getLDAP()->query('uidExists', [$uid, "account"])
+        ) return false;
 
-            // Check if user is valid and/or given password is correct
-            if (!$this->model->getLDAP()->query('bind', [$user_dn, $prev_password])) return false;
+        // Get user DN
+        $user_dn = $this->model->getLDAP()->query('getDnByUid', [$uid]);
 
-            // Change password
-            if (!$this->model->getLDAP()->query('changeUserPassword', [$user_dn, $new_password])) return false;
+        // Check if user is valid and/or given password is correct
+        if (!$this->model->getLDAP()->query('bind', [$user_dn, $prev_password])) {
+            $this->err_msg = 'Incorrect Password';
+            return false;
+        } 
 
-            return true;
-        } else {
-            die('Connection to LDAP service failed');
+        // Change password
+        if (!$this->model->getLDAP()->query('changeUserPassword', [$user_dn, $new_password])) return false;
+
+        // Edit Last Password Change column in Database
+        $group = $this->model->getLDAP()->query('getGroupOfUser', [$uid]);
+        $table = $this->convertGroupToTable($group);
+        $this->model->getDB()->query('updateLastPasswordChange', [$uid, $table]);
+
+        return true;
+    }
+
+    private function convertGroupToTable($group)
+    {
+        switch ($group) {
+            case 'patienten':
+                return 'Patiënt';
+            case 'dokters':
+                return 'Dokter';
+            case 'dietisten':
+                return 'Diëtist';
+            case 'dokters':
+                return 'Dokter';
+            case 'psychologen':
+                return 'Psycholoog';
+            case 'fysiotherapeuten':
+                return 'Fysiotherapeut';
+            case 'administrators':
+                return 'Admin';
+            default:
+                return '';
         }
     }
 
-    protected function validatePasswordTest($prev_password, $new_password)
+    private function validatePassword($prev_password, $new_password)
     {
         // Check if passwords are the same 
         if ($prev_password == $new_password) {
-            $this->err_msg = 'New Password can\'t be Previous Password';
+            $this->err_msg = 'New Password can\'t be the same as the old Password';
             return false;
         }
 
@@ -58,19 +102,19 @@ class ChangePassword extends Controller
 
         // Length of password should be 8 characters or longer
         if (strlen($new_password) < 8) {
-            $this->err_msg = 'New Password needs a minimum length of 8 characters';
+            $this->err_msg = 'New Password needs to be at least 8 characters long';
             return false;
         }
 
         // Password should contain atleast one capital letter
         if (!$this->isPartUppercase($new_password)) {
-            $this->err_msg = 'New Password should contain atleast one capital letter';
+            $this->err_msg = 'New Password needs to contain atleast one capital letter';
             return false;
         }
 
         // Password should contain atleast one special character
         if (!$this->hasSpecialCharacter($new_password)) {
-            $this->err_msg = 'New Password should contain atleast one special character';
+            $this->err_msg = 'New Password needs to contain atleast one special character';
             return false;
         }
 

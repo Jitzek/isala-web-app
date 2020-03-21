3bc7b4181ef57f8e96a0c82f05cc60c2b2b840f3
Jitzek <Jitzek@users.noreply.github.com>
2020-03-21 18:20:53 +0100

    Added Authentication Model
    - Model contains all data needed to authenticate User

---
+++ isala.local/app/models/AuthenticationModel.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('../app/database/connection.php');
+require_once('../app/ldap/connection.php');
+
+class AuthenticationModel
+{
+    private $db;
+    private $ldap;
+    public function __construct()
+    {
+        $this->db = new DBConnection();
+        $this->ldap = new LDAPConnection();
+    }
+
+    public function getDB()
+    {
+        return $this->db;
+    }
+
+    public function getLDAP()
+    {
+        return $this->ldap;
+    }
+}

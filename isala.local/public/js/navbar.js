a8cd92dcf36e67b6875349c674ace46dabac32bb
Jitzek <Jitzek@users.noreply.github.com>
2020-03-25 16:10:40 +0100

    Fixed Navbar Issues
    - Switching from mobile phone to desktop view would sometimes hide the sidebar.
    - Added some more links (logo redirects to home and profiel redirects to profile)

--- isala.local/public/js/navbar.js
+++ isala.local/public/js/navbar.js
@@ -1,15 +1,17 @@
 function sidebar(x) {
   if (x.matches) {
-      document.getElementById("sidebarCollapse").style.display = "inline";
-    $("#sidebarCollapse").on("click", function() {
+    document.getElementsByClassName("sidebarCollapse")[0].style.display = "inline";
+    $("#sidebarCollapse").on("click", function () {
       $("#sidebar").toggleClass("active");
     });
   } else {
     $("#sidebarCollapse").off();
-    document.getElementById("sidebarCollapse").style.display = "none"
+    if (document.getElementById('sidebar').className == "active") {
+      $("#sidebar").toggleClass("active");
+    }
+    document.getElementsByClassName("sidebarCollapse")[0].style.display = "none"
   }
 }
 var x = window.matchMedia("(max-width: 768px)");
 sidebar(x);
 x.addListener(sidebar);
-

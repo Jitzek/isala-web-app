5416792a88332b8fd30bbb8dc203c2e8b55bedd8
AnnoyingDog99 <43909641+AnnoyingDog99@users.noreply.github.com>
2020-04-02 20:52:50 +0200

    Removed Ldap

--- isala.local/app/models/UploadModel.php
+++ isala.local/app/models/UploadModel.php
@@ -1,18 +1,14 @@
 <?php
-
-require_once('../app/ldap/connection.php');
 require_once('../app/database/connection.php');
 
 class UploadModel
 {
     private $title;
     private $db;
-    private $ldap;
     public function __construct()
     {
         $this->title = 'Upload';
         $this->db = new DBConnection();
-        $this->ldap = new LDAPConnection();
     }
 
     public function getTitle()
@@ -23,10 +19,5 @@ class UploadModel
     public function getDB()
     {
         return $this->db;
-    }
-
-    public function getLDAP()
-    {
-        return $this->ldap;
     }
 }
